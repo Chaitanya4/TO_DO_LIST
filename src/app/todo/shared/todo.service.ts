645ea06f64ef36b3058524fe4c1f10d1da5102f1
Chaitanya4 <chaitanya707@gmail.com>
2020-07-16 16:31:47 +0530

    todo

---
+++ src/app/todo/shared/todo.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import {AngularFireDatabase,AngularFireList} from '@angular/fire/database';
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+toDoList: AngularFireList<any>;
+  constructor(private firebasedb: AngularFireDatabase) { }
+
+getToDoList()
+{
+  this.toDoList=this.firebasedb.list('titles');
+  return this.toDoList;
+}
+
+addTitle(title:string)
+{
+  if(title==="")
+  {
+    alert('Please enter a task to the list');
+  
+  }
+  else{
+    this.toDoList.push({
+      title:title,
+      isChecked:false
+    });
+  }
+ 
+}
+
+checkOrUnCheckTitle($key:string, flag:boolean)
+{
+  this.toDoList.update($key,{isChecked:flag});
+}
+
+removeTitle($key:string)
+{
+this.toDoList.remove($key);
+}
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,28 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase:{
+    
+      apiKey: "AIzaSyDS3KHGWCMWYIGNrI2exh3ZFxPU1veRFd8",
+      authDomain: "todolistapp-bcca9.firebaseapp.com",
+      databaseURL: "https://todolistapp-bcca9.firebaseio.com",
+      projectId: "todolistapp-bcca9",
+      storageBucket: "todolistapp-bcca9.appspot.com",
+      messagingSenderId: "467330372276",
+      appId: "1:467330372276:web:e2a54229107ed02fab3de1",
+      measurementId: "G-DKJ6ETVXM6"
+    
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
